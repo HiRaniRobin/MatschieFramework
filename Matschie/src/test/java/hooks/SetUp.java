e591923829ebee8234461daa95c610a3da4097ef
HiRaniRobin <76161503+HiRaniRobin@users.noreply.github.com>
2021-07-18 15:13:44 +0530

    Add files via upload

---
+++ Matschie/src/test/java/hooks/SetUp.java
@@ -0,0 +1,31 @@
+package hooks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class SetUp {
+	
+	@Before
+	public void setUp() throws FileNotFoundException, IOException{ 
+		Properties prop = new Properties();
+		prop.load(new FileInputStream(new File("./src/test/resources/config.properties")));
+		
+		RestAssured.baseURI = "https://"+prop.getProperty("server")+"/"+prop.getProperty("resources")+"/";
+		RestAssured.authentication = RestAssured.basic(prop.getProperty("username"), prop.getProperty("password"));
+		
+	}
+	
+	@After
+	public void tearDown(){ 
+	}
+}
